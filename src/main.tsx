b2383c323e3771215c9d81438f1689ede3e41169
Ackjosh <163001837+Ackjosh@users.noreply.github.com>
2025-10-08 00:28:53 +0530

    removed clerk

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import 'lenis/dist/lenis.css'
-import { ClerkProvider } from '@clerk/clerk-react'
-
-// Import your Publishable Key
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
-
-if (!PUBLISHABLE_KEY) {
-  throw new Error("Missing Publishable Key")
-}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
